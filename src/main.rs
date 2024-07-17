bfb880a0643d7fd1d1e46c43c12b1d4f93dbad3d
Christopher Hicks <chicks.net@gmail.com>
2024-07-16 20:13:22 -0700

    🩻 use glob to get filenames

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::path::Path;
+use glob::glob;
 
 fn main() {
     // get directory argument and verify that it is actually a directory
@@ -12,8 +13,19 @@ fn main() {
 
     // find posts directory
     let posts_path = Path::new(base_path).join("Google+ Stream/Posts");
-    let posts_path_string = posts_path.to_str();
+    let posts_path_string = posts_path.to_str().unwrap();
     assert_eq!(posts_path.exists(), true);
     assert_eq!(posts_path.is_dir(), true);
     println!("Posts are in {posts_path_string:?}");
+
+    // Loop through html files
+    let mut post_pattern: String = (&posts_path_string).to_string();
+    post_pattern.push_str("/*.html");
+    println!("Debug: {post_pattern}");
+    for entry in glob(&post_pattern).expect("Failed to glob") {
+        match entry {
+            Ok(path) => println!("{:?}", path.display()),
+            Err(e) => println!("{:?}", e),
+        }
+    }
 }
